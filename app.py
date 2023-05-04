103a2b06c94237b3121090edbe92fe1cc4e779cf
javi <juliettetobar23@gmail.com>
2023-05-04 16:00:06 -0400

    se agrega main

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hola gente():
+    return render_template("index.html")
+
+@app.route('/reels')
+def hola_inmundo():
+    name = request.args.get("username")
+    lastname = request.args.get("lastname")
+    return render_template("reels.html",name=name,lastname=lastname)
